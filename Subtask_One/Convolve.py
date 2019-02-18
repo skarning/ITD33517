f969d3cb03d5c353271a1ae3b373f3bbce9568d0
skarning <sivertskarning@hotmail.com>
2019-02-18 14:31:20 +0100

    Removed testing comments, added functionality in Convolute function

--- Subtask_One/Convolve.py
+++ Subtask_One/Convolve.py
@@ -13,35 +13,47 @@ def zero_pad(img,flt,pdng_rws,pdng_clms):
     for i in range(pdng_clms,pdng_clms+width):
         for j in range(pdng_rws,pdng_rws+height):
             padded_img[i,j] = img[i-pdng_clms,j-pdng_rws]
-    #Saving image to test zero_padding function
-    io.imsave('lena_out.png',padded_img)
+    return padded_img
 
 def rflct_pad(img,flt,pdng_rws,pdng_clms):
     pass
 
-def convolve(img, flt, pdng_func):
-    pass
+def convolve(img, flt, pdng_func,pdng_rws,pdng_clms):
+    padded_img = pdng_func
 
+    for i in range(pdng_rws,padded_img.shape[0]-pdng_rws):
+        for j in range(pdng_clms,padded_img.shape[1]-pdng_clms):
+            padded_img[j,i] = 0.9
+    io.imsave('lena_out.png',padded_img)
+
+#Script starts here
+if len(sys.argv)<2:
+    print('No image as argument')
+    sys.exit()
+    
 img = util.img_as_float64(color.rgb2gray(io.imread(sys.argv[1])))
 rws = int(input('Number of rows in filter:'))
 clms = int(input('Number of clms in filter'))
 flt = np.zeros((rws,clms))
 
+"""
 for i in range(0,rws):
     for j in range(0,clms):
         flt[i,j] = int(input('Type(Row:{}, column:{}) value: '.format(i,j)))
-
-pdng_rws = (rws-1)/2
-pdng_clms = (clms-1)/2
+"""
+pdng_rws = int((rws-1)/2)
+pdng_clms = int((clms-1)/2)
        
 while(True):
-    pdng_optn = int(input('1.Zero-padding\n2.Reflecting-padding'))
+    pdng_optn = int(input('1.Zero-padding\n2.Reflecting-padding\n: '))
     
-    if(pdng_optn == 1):
+    if pdng_optn == 1:
         pdng_func = zero_pad(img,flt,pdng_rws,pdng_clms)
-        end
-    elif(pdng_optn == 2):
+        break
+    elif pdng_optn == 2:
         pdng_func = rflct_pad(img,flt,pdng_rws,pdng_clms)
-        end
+        break
     else:
         print('{} not an option..'.format(pdng_optn))
+
+convolve(img,flt,pdng_func,pdng_rws,pdng_clms)
