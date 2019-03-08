23d1528119590f77c11efb6f5fcb1b0070e72f12
skarning <sivertskarning@hotmail.com>
2019-03-08 14:05:51 +0100

    added functionality for adding a histogram to a file

--- Subtask_Three/histogram.py
+++ Subtask_Three/histogram.py
@@ -12,6 +12,7 @@ def get_hstg_from_file(fl_path):
     array = []
     for x in fl:
         array.append(int(x))
+    fl.close
     array = np.asarray(array)
     return array
 
@@ -49,7 +50,18 @@ def get_cmltv_prob(array):
         array[i] = array[i]+buff
         buff = array[i]
     return array
-        
+
+
+# Placing a histogram from an image to a file
+def wrt_hstg_to_file(file_path, img):
+    hstg = get_hstg_from_img(img)
+    hstg = np.asarray(hstg)
+    fl = open(file_path, 'w')
+    for i in range(0, len(hstg)):
+        fl.write(str(hstg[i]))
+        fl.write('\n')
+    fl.close
+
 
 # Getting Cumulative function for image
 img = util.img_as_ubyte(color.rgb2gray(io.imread('barbara.png')))
@@ -63,4 +75,5 @@ spc_hstg = get_hstg_from_file('file_histogram.txt')
 spc_hstg_prob = cnv_to_prob(spc_hstg, get_nmb_of_pxls_in_hstg_file(spc_hstg))
 spc_hstg_cmlprob = get_cmltv_prob(spc_hstg_prob)
 
+print(spc_hstg_cmlprob)
 print(img_hstg_cmlprob)
