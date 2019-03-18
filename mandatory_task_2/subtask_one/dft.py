22a708376615a745fff19e7a94d0078bc1468f54
skarning <sivertskarning@hotmail.com>
2019-03-18 12:29:49 +0100

    Updated dft algorithim without succsess

--- mandatory_task_2/subtask_one/dft.py
+++ mandatory_task_2/subtask_one/dft.py
@@ -10,18 +10,20 @@ def imprt_img(img_pth):
 
 # Perform a dft trasnformation on a image
 def dft_trnsf(img):
-    N1 = img.shape[0]
-    N2 = img.shape[1]
-    for k in range(0, img.shape[0]):
-        for l in range(0, img.shape[1]):
+    N = img.shape[0]
+    dft_img = np.zeros((50, 50), dtype=int)
+    for k in range(0, N):
+        for l in range(0, N):
             sum = 0
-            for i in range(0, N1):
-                for j in range(0, N2):
-                    factor =  np.exp(-i*2*np.pi
-                                     * (((i**2) / N1) * ((j**2) / N2)))
-                    sum = sum + img[i, j]*factor
-        img[k, l] = sum
-    return img
+            for i in range(0, N):
+                for j in range(0, N):
+                    exp_fac = -i * 2 * np.pi
+                    exp_frac_fac = ((k*i)/N) * ((l*j)/N)
+                    sum = sum + img[i, j] * np.exp(exp_fac * exp_frac_fac)
+            sum = sum / N
+            dft_img[k, l] = sum
+            print(sum)
+    return dft_img
 
 
 # Save the image
@@ -32,7 +34,10 @@ def save_img(img, fname='my_saved_img.png'):
 if len(sys.argv) < 1:
     print('Not enough arguments')
     exit
-
+    
+#img = np.asmatrix([[255, 255, 0, 255, 255], [255, 255, 0, 255, 255],
+#                  [255, 255, 0, 255, 255], [255, 255, 0, 255, 255]])
 img = imprt_img(sys.argv[1])
 dft_img = dft_trnsf(img)
 save_img(dft_img, 'dft_transform.png')
+dft_img = np.clip(dft_img, 0, 255)
