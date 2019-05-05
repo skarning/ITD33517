8f948f136c5ce634fc2f63a6cc5b359cae0927d1
skarning <sivertskarning@hotmail.com>
2019-05-05 23:54:49 +0200

    Re added subtask 2 and 3

---
+++ mandatory-task-1/subtask-2/histogram.py
@@ -0,0 +1,100 @@
+import scipy
+import numpy as np
+import sys
+import skimage
+from skimage import io, util, color
+from scipy import ndimage
+
+
+# Reads file and returns histogram
+def get_hstg_from_file(fl_path):
+    fl = open(fl_path, 'r')
+    array = []
+    for x in fl:
+        array.append(int(x))
+    fl.close
+    array = np.asarray(array)
+    return array
+
+
+# Returns number of pixels in histogram file
+def get_nmb_of_pxls_in_hstg_file(array):
+    sum = 0
+    for x in array:
+        sum = sum+x
+    return sum
+
+
+# Converts every value in array to probability
+def cnv_to_prob(array, sum):
+    array = array.astype(float)
+    for i in range(len(array)):
+        array[i] = array[i]/sum
+    return array
+
+
+# Reads image and returns array of intensity
+def get_hstg_from_img(img):
+    hstg = np.zeros(256)
+    hstg = hstg.astype(int)
+    for i in range(0, img.shape[0]-1):
+        for j in range(0, img.shape[1]-1):
+            hstg[img[i, j]] += 1
+    return hstg
+
+
+# Returns array with cumulative probability
+def get_cmltv_prob(array):
+    buff = 0
+    for i in range(0, len(array)):
+        array[i] = array[i]+buff
+        buff = array[i]
+    return array
+
+
+# Placing a histogram from an image to a file
+def wrt_hstg_to_file(file_path, img):
+    hstg = get_hstg_from_img(img)
+    hstg = np.asarray(hstg)
+    fl = open(file_path, 'w')
+    for i in range(0, len(hstg)):
+        fl.write(str(hstg[i]))
+        fl.write('\n')
+    fl.close
+
+
+# Matching the histograms CDF
+def match_cdf(spec_hstg, org_hstg):
+    array = [0]*len(org_hstg)
+    for i in range(0, len(org_hstg)):
+        for j in range(0, len(spec_hstg)):
+            if (spec_hstg[j]-org_hstg[i]) >= 0:
+                array[i] = j
+                break
+    return array
+
+
+# Go thorough img and implement new histogram
+def apply_hstg_to_img(img, hstg_mapping):
+    for i in range(0, img.shape[0]):
+        for j in range(0, img.shape[1]):
+            img[i, j] = hstg_mapping[img[i, j]]
+    return img
+
+
+# Getting Cumulative function for image
+img = util.img_as_ubyte(color.rgb2gray(io.imread('barbara.png')))
+nmb_of_pxls = img.shape[0]*img.shape[1]
+img_hstg = get_hstg_from_img(img)
+img_hstg_prob = cnv_to_prob(img_hstg, nmb_of_pxls)
+img_hstg_cmlprob = get_cmltv_prob(img_hstg_prob)
+
+# Getting Cumulateive function for histogram file
+spc_hstg = get_hstg_from_file('file_histogram.txt')
+spc_hstg_prob = cnv_to_prob(spc_hstg, get_nmb_of_pxls_in_hstg_file(spc_hstg))
+spc_hstg_cmlprob = get_cmltv_prob(spc_hstg_prob)
+
+hstg_mapping = match_cdf(spc_hstg_cmlprob, img_hstg_cmlprob)
+spec_img = apply_hstg_to_img(img, hstg_mapping)
+
+io.imsave('hist_spec_image.png', spec_img)
